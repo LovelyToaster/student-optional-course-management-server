aa845ade26b79ff95753c12eb3080b5cbba20153
LovelyToaster94 <LovelyToaster94@outlook.com>
2025-05-09 13:16:22 +0800

    refactor(Controller): 优化异常处理返回类型

--- src/main/java/com/lovelytoaster94/Controller/HandlerException.java
+++ src/main/java/com/lovelytoaster94/Controller/HandlerException.java
@@ -2,6 +2,8 @@ package com.lovelytoaster94.Controller;
 
 import com.lovelytoaster94.Until.Code;
 import com.lovelytoaster94.Until.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +13,9 @@ public class HandlerException {
 
     @ExceptionHandler({Exception.class})
     @ResponseBody
-    public Result handleException(Exception e) {
-        return new Result(Code.SERVICE_FAILED, "发生错误，请联系管理员", e.toString());
+    public ResponseEntity<Result> handleException(Exception e) {
+        Result result = new Result(Code.SERVICE_FAILED, "发生错误，请联系管理员", e.toString());
+
+        return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
