4bd35c9bbbbc390d406fb26cadb13d1e3a018a84
LovelyToaster94 <LovelyToaster94@outlook.com>
2025-05-09 16:15:32 +0800

    feat(exception): 增加数据完整性异常处理

--- src/main/java/com/lovelytoaster94/Controller/HandlerException.java
+++ src/main/java/com/lovelytoaster94/Controller/HandlerException.java
@@ -2,14 +2,26 @@ package com.lovelytoaster94.Controller;
 
 import com.lovelytoaster94.Until.Code;
 import com.lovelytoaster94.Until.Result;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @ControllerAdvice
 public class HandlerException {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseBody
+    public ResponseEntity<Result> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        Throwable rootCause = e.getRootCause();
+
+        Result result = new Result(Code.SERVICE_FAILED, "数据被关联，不能删除或更新。", rootCause != null ? rootCause.getMessage() : e.getMessage());
+        return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+    }
 
     @ExceptionHandler({Exception.class})
     @ResponseBody
